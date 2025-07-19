2667199503f230fe8706c8b0255ee075549d2cb3
Vinicius Santos <144724415+Monntagne@users.noreply.github.com>
2025-07-19 11:53:03 -0300

    Add files via upload

---
+++ DESENVOLVIMENTO - VINICIUS CARLOS DA SILVA SANTOS/AULA 4/Exercicio/Tabuada/script.js
@@ -0,0 +1,10 @@
+
+let multiplicacao = document.getElementById(j)
+resul = multiplicacao *2
+   console.log(resul)
+
+   
+for(let i=1; i <=10; i++){
+    resul = multiplicacao *i
+   console.log(resul)
+}
---
+++ DESENVOLVIMENTO - VINICIUS CARLOS DA SILVA SANTOS/AULA 4/script.js
@@ -0,0 +1,33 @@
+/*for(let i=1; i <=10; i++){
+   console.log(i)
+}
+
+for(let i=1; i <=50; i++){
+   console.log(i)
+}
+for(let i=1; i <=100; i++){
+   console.log(i)
+}
+*/
+/*
+for(let i = 0%2;  i <=20; i+=2){
+        console.log(i)
+    } 
+
+
+
+for(let i =0;  i<=20; i++){
+    if (i%2 == 0){
+        console.log(i)
+    }
+    else(
+        console.log()
+    )
+} 
+  
+*/
+let soma =0
+for(let i=1; i<=100; i++){
+    soma+=i 
+   console.log(soma)
+}
