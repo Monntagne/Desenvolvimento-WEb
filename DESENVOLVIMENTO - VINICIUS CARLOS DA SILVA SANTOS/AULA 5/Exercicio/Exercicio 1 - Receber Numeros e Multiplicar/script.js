a941b1ead65841e3ba55bf0106d9cff0ec8ff54c
Vinicius Santos <144724415+Monntagne@users.noreply.github.com>
2025-07-26 11:28:32 -0300

    Add files via upload

---
+++ DESENVOLVIMENTO - VINICIUS CARLOS DA SILVA SANTOS/AULA 5/Exercicio/Exercicio 1 - Receber Numeros e Multiplicar/script.js
@@ -0,0 +1,7 @@
+
+function mult (numero1, numero2){
+    return numero1 * numero2
+}
+let resultado = mult(Number(prompt(`Digite um numero:`)),Number(prompt(`Digite mais um numero:`)))
+console.log(resultado) 
+
---
+++ DESENVOLVIMENTO - VINICIUS CARLOS DA SILVA SANTOS/AULA 5/script.js
@@ -0,0 +1,49 @@
+function ola(){
+    console.log(`Olá`)
+}
+//ola();
+
+
+function bomdia(){
+    console.log(`Bom dia!!`)
+}
+//bomdia();
+
+
+function olaNome(nome){
+    console.log(`Olá ${nome}`)
+}
+//olaNome(prompt(`Digite Seu nome!`))
+
+
+function soma(numero1, numero2){
+    return numero1 + numero2
+}
+let resultadoSoma = soma(15, 15)
+console.log(resultadoSoma)
+
+
+function subtracao(numero3, numero4){
+    return numero3 - numero4
+}
+let resultadoSubtracao = subtracao(5, 10)
+console.log(resultadoSubtracao)
+
+
+function multiplicacao(numero5, numero6){
+    return numero5 * numero6
+}
+let resultadoMultiplicacao = multiplicacao(5, 8)
+console.log(resultadoMultiplicacao)
+
+
+function divisao(numero7, numero8){
+    return numero7 / numero8 
+}
+let resultadoDivisao = divisao(125, 5)
+console.log(resultadoDivisao)
+
+
+
+
+
